54bc7e9f7a738710983d97460ecf1b1b24cc6a8e
Dario Karanovi <kleetsonfire@gmail.com>
2021-02-10 22:03:56 -0500

    Create pathfinderAbilityCalc.py

---
+++ level 7/pathfinderAbilityCalc.py
@@ -0,0 +1,13 @@
+""" You've decided to try to create a test framework that allows for easy testing of Pathfinder Ability Score arrays, 
+and their validity, using 25 points.
+
+You will write a function that takes an array of 6 scores (integers) and will return a boolean if they add up in 25 points or less.
+
+Each score will be in range 1 <= x <= 20. You must comply with the table below, and values where x < 7 and x > 18 should return False.
+
+Each score costs a certain amount of points. For example, a score of 16 costs 10 points, but a score of 7 would give 4 back. 
+
+
+Table: https://db4sgowjqfwig.cloudfront.net/campaigns/145740/assets/635596/abilityScoreCosts.png?1472672582
+"""
+
